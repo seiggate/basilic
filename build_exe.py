7869485fcee9a64b8574dbca47b34cfe21d3d9d2
seiggate <76473660+seiggate@users.noreply.github.com>
2025-12-09 17:47:55 +0100

    Updated build_exe.py

--- build_exe.py
+++ build_exe.py
@@ -5,13 +5,15 @@ from pathlib import Path
 project_root = Path(__file__).parent
 client_py = project_root / "client_py"
 
+separator = ';' if sys.platform == 'win32' else ':'
+
 PyInstaller.__main__.run([
     str(client_py / "main.py"),
     '--name=Basilic',
     '--onefile',
     '--windowed',
-    f'--add-data={project_root / ".env"}{";." if sys.platform == "win32" else ":"}.',
-    f'--add-data={client_py / "ui"}{";ui" if sys.platform == "win32" else ":"}ui',
+    f'--add-data={project_root / ".env"}{separator}.',
+    f'--add-data={client_py / "ui"}{separator}ui',
     '--icon=NONE',
     '--clean',
     '--noconfirm',
