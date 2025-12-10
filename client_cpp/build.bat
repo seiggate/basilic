893cd072a42c214992cce5c41bb4512e295491de
seiggate <76473660+seiggate@users.noreply.github.com>
2025-12-10 19:18:47 +0100

    Updated test_build.bat

--- client_cpp/build.bat
+++ client_cpp/build.bat
@@ -3,6 +3,12 @@
 echo Building Basilic C++/Qt Client
 echo.
 
+REM Set Qt6 path
+set "QT_DIR=C:\Qt\6.10.1\mingw_64"
+set "PATH=%QT_DIR%\bin;%PATH%"
+set "Qt6_DIR=%QT_DIR%\lib\cmake\Qt6"
+set "CMAKE_PREFIX_PATH=%QT_DIR%"
+
 if not exist "build" (
     echo Creating build directory...
     mkdir build
@@ -11,14 +17,10 @@ if not exist "build" (
 cd build
 
 echo Running CMake...
-cmake .. -G "Visual Studio 17 2022"
-if errorlevel 1 (
-    echo CMake failed! Trying with MinGW...
-    cmake .. -G "MinGW Makefiles"
-)
+cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH% -DQt6_DIR=%Qt6_DIR%
 
 echo Compiling...
-cmake --build . --config Release
+mingw32-make
 
 echo.
 echo Build complete!
--- client_cpp/test_build.bat
+++ client_cpp/test_build.bat
@@ -3,6 +3,12 @@ echo ==================================
 echo Testing Basilic C++ Models
 echo ==================================
 
+REM Set Qt6 path
+set "QT_DIR=C:\Qt\6.10.1\mingw_64"
+set "PATH=%QT_DIR%\bin;%PATH%"
+set "Qt6_DIR=%QT_DIR%\lib\cmake\Qt6"
+set "CMAKE_PREFIX_PATH=%QT_DIR%"
+
 REM Create build directory
 if not exist build_test mkdir build_test
 cd build_test
@@ -10,12 +16,12 @@ cd build_test
 REM Configure with CMake
 echo.
 echo Configuring CMake...
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH% -DQt6_DIR=%Qt6_DIR%
 
 REM Build only the test executable
 echo.
 echo Building test executable...
-cmake --build . --target test_models --config Debug
+mingw32-make test_models
 
 REM Run tests if build succeeded
 if %ERRORLEVEL% EQU 0 (
